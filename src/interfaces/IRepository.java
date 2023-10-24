40eb64255c10af4955df02b5ac438a3e5515a198
Byron Jimenez <byronjimenez9911@gmail.com>
2023-10-23 19:51:02 -0600

    IRepository get model by id (index)

--- src/interfaces/IRepository.java
+++ src/interfaces/IRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface IRepository {
     List<Object> get();
+    Object getById(int id);
     void create(Object object);
     void update(Object object, int id);
     void delete(int id);
