e1cfb622e5585159d0f4bff842c2c76d568c86bb
Byron Jimenez <byronjimenez9911@gmail.com>
2023-10-23 20:57:31 -0600

    Menu handler created

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // menu de opciones para crear razas, categorias y pacientes
+        // Inicializar el scanner
         Scanner scanner = new Scanner(System.in);
-
-        // Inicializacion de repositorios (Modelo temporal para pruebas, luego se hara la im=nnterfaz y se creara persistencia de datos)
-        RazaRepository razaRepository = new RazaRepository();
-        CategoriaRepository categoriaRepository = new CategoriaRepository();
-        PacienteRepository pacienteRepository = new PacienteRepository();
-        CitaRepository citaRepository = new CitaRepository();
-        ExpedienteRepository expedienteRepository = new ExpedienteRepository();
-        VacunaRepository vacunaRepository = new VacunaRepository();
-        MedicinaRepository medicinaRepository = new MedicinaRepository();
+        // Inicializar el manjeador del menu
+        MenuHandler menuHandler = new MenuHandler();
 
         int opcion = 0;
 
@@ -47,175 +40,43 @@ public class Main {
             try {
                 switch (opcion) {
                     case 1:
-                        System.out.print("Nombre de la raza: ");
-                        String nombreRaza = scanner.nextLine();
-                        Raza raza = new Raza(nombreRaza);
-                        razaRepository.create(raza);
+                        menuHandler.crearRaza();
                         break;
                     case 2:
-                        System.out.print("Nombre de la categoria: ");
-                        String nombreCategoria = scanner.nextLine();
-                        Categoria categoria = new Categoria(nombreCategoria);
-                        // Guardar en el repositorio
-                        categoriaRepository.create(categoria);
+                        menuHandler.crearCategoria();
                         break;
                     case 3:
-                        // Nombre del paciente
-                        System.out.print("Nombre del paciente: ");
-                        String nombrePaciente = scanner.nextLine();
-
-                        // Raza del paciente
-                        System.out.print("Elige la raza: \n");
-                        List<Object> razas = razaRepository.get();
-                        for (int i = 0; i < razas.size(); i++) {
-                            System.out.println(i + ". " + ((Raza) razas.get(i)).getNombre());
-                        }
-                        String razaIndex = scanner.nextLine();
-                        Object razaObject = razas.get(Integer.parseInt(razaIndex));
-
-                        // Categoria del paciente
-                        System.out.print("Elije la categoria: \n");
-                        List<Object> categorias = categoriaRepository.get();
-                        for (int i = 0; i < categorias.size(); i++) {
-                            System.out.println(i + ". " + ((Categoria) categorias.get(i)).getNombre());
-                        }
-                        String categoriaIndex = scanner.nextLine();
-                        Object categoriaObject = categorias.get(Integer.parseInt(categoriaIndex));
-
-                        // Nombre del dueño
-                        System.out.print("Nombre del dueño: ");
-                        String nombreDueno = scanner.nextLine();
-
-                        // Edad del paciente
-                        System.out.print("Edad del paciente: ");
-                        int edad = scanner.nextInt();
-
-                        // Sexo del paciente
-                        System.out.print("Sexo del paciente: ");
-                        String sexo = scanner.nextLine();
-
-                        // Fecha de inscripcion, se obtiene la fecha actual
-                        Date fechaActual = new Date();
-                        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-                        String fechaActualString = formato.format(fechaActual);
-
-                        // Altura del paciente
-                        System.out.print("Altura del paciente: ");
-                        double altura = scanner.nextDouble();
-
-                        // Peso del paciente
-                        System.out.print("Peso del paciente: ");
-                        double peso = scanner.nextDouble();
-
-                        // ID unico del paciente
-                        System.out.print("ID unico del paciente: ");
-                        String idUnico = scanner.nextLine();
-
-                        // Pelaje del paciente
-                        System.out.print("Pelaje del paciente: ");
-                        String pelaje = scanner.nextLine();
-
-                        // Fecha de nacimiento del paciente
-                        System.out.print("Fecha de nacimiento del paciente: ");
-                        String fechaNacimiento = scanner.nextLine();
-
-                        // Crear el paciente
-                        Paciente paciente = new Paciente(nombrePaciente, nombreDueno, edad, (Categoria) categoriaObject, (Raza) razaObject, sexo, fechaActualString, altura, peso, idUnico, pelaje, fechaNacimiento);
-                        pacienteRepository.create(paciente);
+                        menuHandler.crearPaciente();
                         break;
                     case 4:
-                        // Crear cita
-                        // Dia de la cita
-                        System.out.print("Dia de la cita: ");
-                        String diaCita = scanner.nextLine();
-
-                        // Hora de la cita
-                        System.out.print("Hora de la cita: ");
-                        String horaCita = scanner.nextLine();
-
-                        // Elegir paciente
-                        System.out.print("Elije el paciente: \n");
-                        List<Object> pacientesList = pacienteRepository.get();
-                        for (int i = 0; i < pacientesList.size(); i++) {
-                            System.out.println(i + ". " + ((Paciente) pacientesList.get(i)).getNombre());
-                        }
-                        String pacienteIndex = scanner.nextLine();
-                        Object pacienteObject = pacientesList.get(Integer.parseInt(pacienteIndex));
-
-                        // Motivo de la cita
-                        System.out.print("Motivo de la cita: ");
-                        String motivoCita = scanner.nextLine();
-
-                        // Crear la cita
-                        Cita cita = new Cita(diaCita, horaCita, (Paciente) pacienteObject, motivoCita);
-                        citaRepository.create(cita);
+                        menuHandler.crearCita();
                         break;
                     case 5:
-                        // Crear vacuna
-                        // Nombre de la vacuna
-                        System.out.print("Nombre de la vacuna: ");
-                        String nombreVacuna = scanner.nextLine();
-
-                        // Descripcion de la vacuna
-                        System.out.print("Descripcion de la vacuna: ");
-                        String descripcionVacuna = scanner.nextLine();
-
-                        // Crear la vacuna
-                        Vacuna vacuna = new Vacuna(nombreVacuna, descripcionVacuna);
-                        vacunaRepository.create(vacuna);
+                        menuHandler.crearVacuna();
                         break;
                     case 6:
-                        // Crear medicina
-                        // Nombre de la medicina
-                        System.out.print("Nombre de la medicina: ");
-                        String nombreMedicina = scanner.nextLine();
-
-                        // Crear la medicina
-                        Medicina medicina = new Medicina(nombreMedicina);
-                        medicinaRepository.create(medicina);
+                        menuHandler.crearMedicina();
                         break;
                     case 7:
-                        List<Object> pacientes = pacienteRepository.get();
-                        for (Object o : pacientes) {
-                            ((Paciente) o).show();
-                        }
+                        menuHandler.verPacientes();
                         break;
                     case 8:
-                        List<Object> razasList = razaRepository.get();
-                        for (Object o : razasList) {
-                            ((Raza) o).show();
-                        }
+                        menuHandler.verRazas();
                         break;
                     case 9:
-                        List<Object> categoriasList = categoriaRepository.get();
-                        for (Object o : categoriasList) {
-                            ((Categoria) o).show();
-                        }
+                        menuHandler.verCategorias();
                         break;
-
                     case 10:
-                        List<Object> citasList = citaRepository.get();
-                        for (Object o : citasList) {
-                            ((Cita) o).show();
-                        }
+                        menuHandler.verCitas();
                         break;
                     case 11:
-                        List<Object> expedientesList = expedienteRepository.get();
-                        for (Object o : expedientesList) {
-                            ((Expediente) o).show();
-                        }
+                        menuHandler.verExpedientes();
                         break;
                     case 12:
-                        List<Object> vacunasList = vacunaRepository.get();
-                        for (Object o : vacunasList) {
-                            ((Vacuna) o).show();
-                        }
+                        menuHandler.verVacunas();
                         break;
                     case 13:
-                        List<Object> medicinasList = medicinaRepository.get();
-                        for (Object o : medicinasList) {
-                            ((Medicina) o).show();
-                        }
+                        menuHandler.verMedicinas();
                         break;
                     case 14:
                         System.out.println("Adios");
